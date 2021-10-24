11868bcdcd3c2a31b0598ffa9eb85e6ed1a8e881
DPLIT\haris.i <haris.i@dplit.com>
2021-10-24 21:32:38 +0500

    updated ui

--- js/mynav.js
+++ js/mynav.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
     $(".top-btn").hide()
+    $("#nav-open-btn").hide()
     $(window).scroll(function() {
       if ($(window).scrollTop() > 300) {
         $(".top-btn").show("slow");
@@ -7,15 +8,27 @@ $(document).ready(function(){
         $(".top-btn").hide("slow")
       }
     });
-    $(".nav-btn").click(function(){
-      if($("#nav-btn-span").attr('class').includes("fa-times")){
-        $("#nav-btn-span").removeClass("fa-times");
-        $("#nav-btn-span").addClass("fa-bars");
-      }else{
-        $("#nav-btn-span").removeClass("fa-bars");
-        $("#nav-btn-span").addClass("fa-times");
-      }
+    $("#nav-open-btn").click(function(){
+      // if($("#nav-btn-span").attr('class').includes("fa-times")){
+      //   $("#nav-btn-span").removeClass("fa-times");
+      //   $("#nav-btn-span").addClass("fa-bars");
+      // }else{
+      //   $("#nav-btn-span").removeClass("fa-bars");
+      //   $("#nav-btn-span").addClass("fa-times");
+      // }
       
-      $(".nav-list").toggle("slow");
+      $(".nav-div").show("slow");
+      $("#nav-open-btn").hide("slow");
+      setTimeout(() => {
+        $(".nav-list").show("slow");
+      },300)
+    });  
+    $("#nav-close-btn").click(function(){
+      $(".nav-list").hide("slow");
+      $("#nav-open-btn").hide("slow");
+      setTimeout(() => {
+        $(".nav-div").hide("slow");
+      },300)
+      $("#nav-open-btn").show("slow");
     });  
 });
