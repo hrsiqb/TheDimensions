494e3a331b2c97f5f2316f6ad654218737d66d4b
DPLIT\haris.i <haris.i@dplit.com>
2021-10-24 18:28:50 +0500

    updated ui

--- js/mynav.js
+++ js/mynav.js
@@ -1,5 +1,12 @@
 $(document).ready(function(){
-  
+    $(".top-btn").hide()
+    $(window).scroll(function() {
+      if ($(window).scrollTop() > 300) {
+        $(".top-btn").show("slow");
+      } else {
+        $(".top-btn").hide("slow")
+      }
+    });
     $(".nav-btn").click(function(){
       if($("#nav-btn-span").attr('class').includes("fa-times")){
         $("#nav-btn-span").removeClass("fa-times");
