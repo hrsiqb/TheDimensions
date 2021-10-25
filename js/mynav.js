60b03d7cfa58716e1e3a639c33e5ee597d6961ce
DPLIT\haris.i <haris.i@dplit.com>
2021-10-26 00:05:53 +0500

    removed firebase files

--- js/mynav.js
+++ js/mynav.js
@@ -1,34 +1,33 @@
-$(document).ready(function(){
-    $(".top-btn").hide()
+$(document).ready(function () {
+  $(".top-btn").hide()
+  if ($(window).width() <= 700) {
+    $("#nav-open-btn").show()
+    $(".nav-div").hide();
+  }
+  else {
     $("#nav-open-btn").hide()
-    $(window).scroll(function() {
-      if ($(window).scrollTop() > 300) {
-        $(".top-btn").show("slow");
-      } else {
-        $(".top-btn").hide("slow")
-      }
-    });
-    $("#nav-open-btn").click(function(){
-      // if($("#nav-btn-span").attr('class').includes("fa-times")){
-      //   $("#nav-btn-span").removeClass("fa-times");
-      //   $("#nav-btn-span").addClass("fa-bars");
-      // }else{
-      //   $("#nav-btn-span").removeClass("fa-bars");
-      //   $("#nav-btn-span").addClass("fa-times");
-      // }
-      
-      $(".nav-div").show("slow");
-      $("#nav-open-btn").hide("slow");
-      setTimeout(() => {
-        $(".nav-list").show("slow");
-      },300)
-    });  
-    $("#nav-close-btn").click(function(){
-      $(".nav-list").hide("slow");
-      $("#nav-open-btn").hide("slow");
-      setTimeout(() => {
-        $(".nav-div").hide("slow");
-      },300)
-      $("#nav-open-btn").show("slow");
-    });  
+    $(".nav-div").show("slow");
+  }
+  $(window).scroll(function () {
+    if ($(window).scrollTop() > 300) {
+      $(".top-btn").show("slow");
+    } else {
+      $(".top-btn").hide("slow")
+    }
+  });
+  $("#nav-open-btn").click(function () {
+    $(".nav-div").show("slow");
+    $("#nav-open-btn").hide("slow");
+    setTimeout(() => {
+      $(".nav-list").show("slow");
+    }, 300)
+  });
+  $("#nav-close-btn").click(function () {
+    $(".nav-list").hide("slow");
+    $("#nav-open-btn").hide("slow");
+    setTimeout(() => {
+      $(".nav-div").hide("slow");
+    }, 300)
+    $("#nav-open-btn").show("slow");
+  });
 });
